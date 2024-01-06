668db0f02ab024caea1546947754c58cf9b512dc
Posi+ive <positive105@proton.me>
2024-01-06 08:28:44 +0800

    Added many many stuffs

--- device.mk
+++ device.mk
@@ -6,6 +6,16 @@
 #
 
 LOCAL_PATH := device/infinix/X6831
+
+PRODUCT_TARGET_VNDK_VERSION := 33
+
+PRODUCT_SHIPPING_API_VERSION := 33
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -15,17 +25,14 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6768
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6768 \
-    libgptutils \
-    libz \
-    libcutils
+#    bootctrl.mt6768
+     bootctrl \
+     bootctrl.recovery \
+     libmtk_bsg
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -33,3 +40,7 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
